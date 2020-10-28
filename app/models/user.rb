0cb96d3b7721c7d0b2b74ce7599bc7ae197693a2
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-28 12:47:15 +0900

    userモデルファイルの見た目の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :nickname, presence: true
 
   belongs_to :fan_team
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
