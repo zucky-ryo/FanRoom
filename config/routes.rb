c04e6648505379061cdfe397e08ccde0b27a46a1
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-29 00:32:03 +0900

    usersコントローラーのshowアクションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+            registrations: "users/registrations",
+          }
   root to: "private_rooms#index"
+  resources :users, only: :show
 end
