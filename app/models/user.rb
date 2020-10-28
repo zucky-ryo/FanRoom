6fc906874b535784fd7a517feb0a433223179c37
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-28 12:34:07 +0900

    userモデルのテストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    association :fan_team
+  end
+end
