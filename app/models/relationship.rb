782dd8dfa785dc0ba70d129ee4ee6fb7c9c04a4f
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-31 16:44:56 +0900

    アソシエーションの実装

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
 class Relationship < ApplicationRecord
+  belongs_to :user
+  belongs_to :follow, class_name: "User"
+
+  with_options presence: true do
+    validates :user_id
+    validates :follow_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   belongs_to :fan_team
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverse_relationships, source: :user
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
