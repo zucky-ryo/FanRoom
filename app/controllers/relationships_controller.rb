b5d1c8f43f3eb80bfb253858b8ea8c535d62ba6f
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-01 20:22:52 +0900

    フォロー時のプロフィール画面変化の実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,21 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:format])
     following = current_user.follow(@user)
-    following.save
-    render json: [id: @user.id]
+    if following.save
+      render json: [id: @user.id]
+    else
+      render json: [id: @user.id], status: 500
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
     unfollowing = current_user.unfollow(@user)
-    unless unfollowing == nil
+    if unfollowing != nil
       unfollowing.destroy
+      render json: [id: @user.id]
+    else
+      render json: [id: @user.id], status: 500
     end
-    render json: [id: @user.id]
   end
 end
--- app/javascript/user/show.js
+++ app/javascript/user/show.js
@@ -3,21 +3,29 @@ $(function(){
   const urlPath = location.pathname;
   if (urlPath.match(/users/) && urlPath.match(/\d/)) {
     // フォローボタンとフォロー解除ボタンを切り替える
-    $('#follow-button').click(() => {
+    $('#follow-button').bind('ajax:success', () => {
+      let followerNum = parseInt($('#user-followers').text());
       $('#follow-button').css('display', 'none');
       $('#unfollow-none').css('display', 'block');
+      $('#user-followers').text(followerNum + 1);
     });
-    $('#unfollow-none').click(() => {
+    $('#unfollow-none').bind('ajax:success', () => {
+      let followerNum = parseInt($('#user-followers').text());
       $('#unfollow-none').css('display', 'none');
       $('#follow-button').css('display', 'block');
+      $('#user-followers').text(followerNum - 1);
     });
-    $('#unfollow-button').click(() => {
+    $('#unfollow-button').bind('ajax:success', () => {
+      let followerNum = parseInt($('#user-followers').text());
       $('#unfollow-button').css('display', 'none');
       $('#follow-none').css('display', 'block');
+      $('#user-followers').text(followerNum - 1);
     });
-    $('#follow-none').click(() => {
+    $('#follow-none').bind('ajax:success', () => {
+      let followerNum = parseInt($('#user-followers').text());
       $('#follow-none').css('display', 'none');
       $('#unfollow-button').css('display', 'block');
+      $('#user-followers').text(followerNum + 1);
     });
   }
 });
