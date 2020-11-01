5dde2d48bb56eb994b916c12d91f31a1fa35617e
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-01 23:34:36 +0900

    重複した記述の削除

--- app/javascript/user/show.js
+++ app/javascript/user/show.js
@@ -2,30 +2,52 @@
 $(function(){
   const urlPath = location.pathname;
   if (urlPath.match(/users/) && urlPath.match(/\d/)) {
-    // フォローボタンとフォロー解除ボタンを切り替える
-    $('#follow-button').bind('ajax:success', () => {
-      let followerNum = parseInt($('#user-followers').text());
-      $('#follow-button').css('display', 'none');
-      $('#unfollow-none').css('display', 'block');
-      $('#user-followers').text(followerNum + 1);
+    // フォローボタンとフォロー解除ボタンを切り替えて、リストに追加・削除する
+    const followBtn = $('#follow-button');
+    const unfollowBtn = $('#unfollow-button');
+    const followers = $('#user-followers');
+
+    followBtn.bind('ajax:success', () => {
+      let followerNum = parseInt(followers.text());
+      followBtn.css('display', 'none');
+      unfollowBtn.css('display', 'block');
+      followers.text(followerNum + 1);
     });
-    $('#unfollow-none').bind('ajax:success', () => {
-      let followerNum = parseInt($('#user-followers').text());
-      $('#unfollow-none').css('display', 'none');
-      $('#follow-button').css('display', 'block');
-      $('#user-followers').text(followerNum - 1);
+    unfollowBtn.bind('ajax:success', () => {
+      let followerNum = parseInt(followers.text());
+      unfollowBtn.css('display', 'none');
+      followBtn.css('display', 'block');
+      followers.text(followerNum - 1);
     });
-    $('#unfollow-button').bind('ajax:success', () => {
-      let followerNum = parseInt($('#user-followers').text());
-      $('#unfollow-button').css('display', 'none');
-      $('#follow-none').css('display', 'block');
-      $('#user-followers').text(followerNum - 1);
+
+
+    // フォロー/フォロワーをクリックした時リストを表示させる
+    const followingUser = $('#following-user');
+    const followingList = $('#user-following-lists');
+    const followerUser = $('#follower-user');
+    const followerList = $('#user-follower-lists')
+
+    followingUser.click(() => {
+      if (followingList.css('display') == 'none'){
+        followingUser.css('background', 'rgb(253, 54, 28)');
+        followingList.css('display', 'block');
+        followerUser.css('background', 'rgb(255, 123, 0)');
+        followerList.css('display', 'none');
+      }else{
+        followingUser.css('background', 'rgb(255, 123, 0)');
+        followingList.css('display', 'none');
+      }
     });
-    $('#follow-none').bind('ajax:success', () => {
-      let followerNum = parseInt($('#user-followers').text());
-      $('#follow-none').css('display', 'none');
-      $('#unfollow-button').css('display', 'block');
-      $('#user-followers').text(followerNum + 1);
+    followerUser.click(() => {
+      if (followerList.css('display') == 'none'){
+        followerUser.css('background', 'rgb(253, 54, 28)');
+        followerList.css('display', 'block');
+        followingUser.css('background', 'rgb(255, 123, 0)');
+        followingList.css('display', 'none');
+      }else{
+        followerUser.css('background', 'rgb(255, 123, 0)');
+        followerList.css('display', 'none');
+      }
     });
   }
 });
