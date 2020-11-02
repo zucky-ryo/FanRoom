55eead01664aa92b85ce8d005f54df44f2217ce4
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-02 15:30:13 +0900

    nicknameが重複するときのテストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: { case_sensitive: true }
 
   belongs_to :fan_team
   # こちらからフォローしているユーザー（中間テーブルとUsersテーブル）
