8ae9c8dade77e849619bae059c1f691ff9729522
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-03 14:37:47 +0900

    open_room_messagesテーブルの実装

---
+++ app/models/open_message.rb
@@ -0,0 +1,2 @@
+class OpenMessage < ApplicationRecord
+end
---
+++ db/migrate/20201103053523_create_open_messages.rb
@@ -0,0 +1,10 @@
+class CreateOpenMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :open_messages do |t|
+      t.text :content, null: false
+      t.references :user,      foreign_key: true
+      t.references :open_room, foreign_key: true
+      t.timestamps
+    end
+  end
+end
