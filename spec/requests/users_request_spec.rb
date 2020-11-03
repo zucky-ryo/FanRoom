d48b94a827f155389103ea0206d5b5cb1a1ecd87
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-03 12:45:37 +0900

    usersコントローラーのテストコードの実装

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,21 @@
-require 'rails_helper'
-
-RSpec.describe "Users", type: :request do
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+require "rails_helper"
+describe UsersController, type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
+  describe "GET #show" do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get user_path(@user.id)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスにユーザーのnicknameが存在する' do
+      get user_path(@user.id)
+      expect(response.body).to include @user.nickname
+    end
+    it 'showアクションにリクエストするとレスポンスにユーザーのファンチーム名が存在する' do
+      get user_path(@user.id)
+      expect(response.body).to include @user.fan_team.team_name
     end
   end
-
 end
