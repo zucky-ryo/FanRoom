b453fb3286c08cfb76d02c3ebb993e79b30c5d1d
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-07 23:34:33 +0900

    別ルームのメッセージ送信時にメッセージを表示しないように調整

--- app/javascript/channels/open_message_channel.js
+++ app/javascript/channels/open_message_channel.js
@@ -11,39 +11,42 @@ consumer.subscriptions.create("OpenMessageChannel", {
 
   // メッセージ送信時にすぐ表示（本人が送信した場合と他人が送信した場合に表示を分ける）
   received(data) {
-    const japanStandardTime = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
-    const hours = new Date(japanStandardTime).getHours();
-    const minutes = new Date(japanStandardTime).getMinutes().toString().padStart(2, '0');
-    const userId = data.content.user_id;
-    const currentId = parseInt(document.getElementById('id-hidden').textContent);
-    if (userId == currentId) {
-      let html = `<li class="my-message-list">` +
-                      `<div class="my-message-main">` +
-                        `<p class="my-message-time">${hours}:${minutes}</p>` +
-                        `<p class="my-message-box">${data.content.content}</p>` +
+    const urlPath = location.pathname;
+    if (urlPath == '/open_rooms/' + data.content.open_room_id.toString()) {
+      const japanStandardTime = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
+      const hours = new Date(japanStandardTime).getHours();
+      const minutes = new Date(japanStandardTime).getMinutes().toString().padStart(2, '0');
+      const userId = data.content.user_id;
+      const currentId = parseInt(document.getElementById('id-hidden').textContent);
+      if (userId == currentId) {
+        let html = `<li class="my-message-list">` +
+                        `<div class="my-message-main">` +
+                          `<p class="my-message-time">${hours}:${minutes}</p>` +
+                          `<p class="my-message-box">${data.content.content}</p>` +
+                        `</div>` +
+                      `</li>`;
+        let messages = document.getElementById('open-message-lists');
+        let newMessage = document.getElementById('open_message_content');
+        messages.insertAdjacentHTML('beforeend', html);
+        newMessage.value='';
+        // メッセージ送信時、一番下にスクロールする
+        $('#scroll').prop({ scrollTop: $("#scroll").prop("scrollHeight") });
+      } else {
+        let html = `<li class="message-list">` +
+                      `<p class="message-user">${data.user.nickname}</p>` +
+                      `<div class="message-main">` +
+                        `<p class="message-box">${data.content.content}</p>` +
+                        `<p class="message-time">${hours}:${minutes}</p>` +
                       `</div>` +
                     `</li>`;
-      let messages = document.getElementById('open-message-lists');
-      let newMessage = document.getElementById('open_message_content');
-      messages.insertAdjacentHTML('beforeend', html);
-      newMessage.value='';
-      // メッセージ送信時、一番下にスクロールする
-      $('#scroll').prop({ scrollTop: $("#scroll").prop("scrollHeight") });
-    } else {
-      let html = `<li class="message-list">` +
-                    `<p class="message-user">${data.user.nickname}</p>` +
-                    `<div class="message-main">` +
-                      `<p class="message-box">${data.content.content}</p>` +
-                      `<p class="message-time">${hours}:${minutes}</p>` +
-                    `</div>` +
-                  `</li>`;
-      let messages = document.getElementById('open-message-lists');
-      let newMessage = document.getElementById('open_message_content');
-      messages.insertAdjacentHTML('beforeend', html);
-      newMessage.value='';
-      // 他のメンバーのメッセージ送信時、スクロールが一番下であれば一番下にスクロールする
-      if ($('#inner-scroll').innerHeight() == $('#scroll').scrollTop() + 620) {
-        $('#scroll').prop({ scrollTop: $("#scroll").prop("scrollHeight") });
+        let messages = document.getElementById('open-message-lists');
+        let newMessage = document.getElementById('open_message_content');
+        messages.insertAdjacentHTML('beforeend', html);
+        newMessage.value='';
+        // 他のメンバーのメッセージ送信時、スクロールが一番下であれば一番下にスクロールする
+        if ($('#inner-scroll').innerHeight() == $('#scroll').scrollTop() + 620) {
+          $('#scroll').prop({ scrollTop: $("#scroll").prop("scrollHeight") });
+        }
       }
     }
   }
