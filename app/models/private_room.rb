861c2705c0c0c510753d4c06d91c8502d0c5cfc1
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-16 15:40:53 +0900

    アソシエーションの実装

--- app/models/private_room.rb
+++ app/models/private_room.rb
@@ -1,2 +1,5 @@
 class PrivateRoom < ApplicationRecord
+  has_many :private_room_users
+  has_many :users, through: :private_room_users, dependent: :destroy
+  has_many :private_messages, dependent: :destroy
 end
