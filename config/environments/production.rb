962491a52d1633fa6dd48a08fd5dab93ef7cef44
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-11-29 21:03:39 +0900

    本番環境でのActionCableの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = "ws://52.197.109.116/cable"
   config.action_cable.allowed_request_origins = ["http://example.com", /http:\/\/example.*/, /http:\/\/.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
