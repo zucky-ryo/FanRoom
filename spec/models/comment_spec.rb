f192ef490a1365a0f141c7339d64c49182751e25
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-12-30 18:36:04 +0900

    Tweetモデルのテストコードの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメント機能' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    context 'コメント投稿がうまく行く時' do
+      it 'コメントが存在する時投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント投稿が失敗する時' do
+      it 'コメントが存在しない時投稿できない' do
+        @comment.content = ""
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("コメントを入力してください")
+      end
+    end
+  end
 end
